7f5db8577ba907316f9a776d0b3ee76c5e82ca2a
Eric Opoku <eric.opoku20@yahoo.com>
2022-05-07 05:40:45 -0400

    test: implement jest

--- portal/.eslintrc.js
+++ portal/.eslintrc.js
@@ -1,6 +1,25 @@
 module.exports = {
-  extends: ['next/core-web-vitals'],
+  extends: [
+    "prettier",
+    'eslint:recommended',
+    "plugin:jest-dom/recommended",
+    'plugin:storybook/recommended',
+    "plugin:@typescript-eslint/recommended",
+    'next/core-web-vitals',
+  ],
   globals: {
     React: 'readonly',
   },
+  overrides: [
+    {
+      files: ["*.stories.@(ts|tsx|js|jsx|mjs|cjs)"],
+      rules: {
+        // example of overriding a rule
+        "storybook/hierarchy-separator": "error"
+      }
+    }
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error"
+  }
 };
